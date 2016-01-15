f9c15cb89497c1c881b5bd90522d881aaa8bf6b3
sheikh-itech <shapheej@gmail.com>
2016-01-15 20:24:07 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
-	fmt.Fprintf(w, "<h1>Hello from Sheikh!</h1>\n")
+	fmt.Fprintf(w, "<h1>Hello from Sheikh! --- Docker</h1>\n")
 }
 
 func main() {
