615fd865739cbe47033f9a0e0dd7848911ecb8af
sheikh-itech <shapheej@gmail.com>
2016-01-16 02:02:59 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,21 +9,6 @@ import (
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
 	fmt.Fprintf(w, "<h1>Hello from Sheikh! --- Published N Build form Dockerfile--golang_job</h1>\n")
-	time1 := time.Now().Second()
-	fmt.Fprintf(w,"<div>---Timer Started---</div>\n")
-	count := 1
-	for time1 <=  time.Now().Second(){
-		fmt.Println(w,time1)
-		if time1 == 59 {
-			fmt.Println(w,"***- ",count," Min. -***\n")
-			count = count+1
-		}
-		time.Sleep(1*time.Second)
-		if time1 > 50 {
-			break;
-		}
-		time1 = time.Now().Second()
-	}
 }
 
 func main() {
